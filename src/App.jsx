9980b88f38a35d11c95d80537a0115df2dba4259
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2023-02-22 16:39:33 +0400

    React app created, components created, images created, pages created

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Gallery from "./pages/gallery/Gallery";
+import NotFound from "./pages/notFound/NotFound";
+import Plans from "./pages/plans/Plans";
+import Trainers from "./pages/trainers/Trainers";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="gallery" element={<Gallery />} />
+        <Route path="plans" element={<Plans />} />
+        <Route path="trainers" element={<Trainers />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import MainHeader from '../../components/MainHeader'
+import './home.css'
+
+const Home = () => {
+  return (
+    <MainHeader />
+  )
+}
+
+export default Home
